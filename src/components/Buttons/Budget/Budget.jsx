2da7c09f14be273ba9e13dc0f4f1b7ca51122bf2
courfedra <franandres93@gmail.com>
2023-03-19 17:25:13 -0300

    create Budget Component

---
+++ src/components/Buttons/Budget/Budget.jsx
@@ -0,0 +1,9 @@
+import "../../../styles/Budget.sass"
+const Budget = () => {
+    return(
+        <div className="budgetButton">
+            <button>Pedir presupuesto</button>
+        </div>
+    )
+}
+export default Budget
