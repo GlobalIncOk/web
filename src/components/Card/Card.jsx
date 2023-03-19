339f422a83fe9432cc3e5df92736786c637a4ccc
cfooostdijk <cfooostdijk@gmail.com>
2023-03-19 18:45:23 -0300

    Single card done

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../styles/Card.sass'
+import { FaAppStore } from "react-icons/fa";
+
+const Card = ({services}) => {
+	return (
+		<div className={'card-container'}>
+      <FaAppStore className={'image'}/>
+      <div className={'middle-title text-center'}><h1>Lorem ipsum</h1></div>
+      <div className={'middle-text text-center'}><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ducimus exercitationem cupiditate possimus ex ipsum</p></div>
+      <div className={'bottom-text'}><p>Ver más</p></div>
+    </div>
+	)
+}
+
+export default Card;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export { default as NavBar } from './NavBar/NavBar';
+export { default as Card } from './Card/Card';
 export { default as Contact } from './Buttons/Contact/Contact';
+export { default as ServicesTitle } from './Titles/Services/ServicesTitle';
