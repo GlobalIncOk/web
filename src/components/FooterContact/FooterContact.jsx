be5066382778782088fb31e250896883267dbe6c
cfooostdijk <cfooostdijk@gmail.com>
2023-04-08 18:47:57 -0300

    corrections

--- src/components/FooterContact/FooterContact.jsx
+++ src/components/FooterContact/FooterContact.jsx
@@ -9,7 +9,7 @@ const FooterContact = () => {
         <p className='title'>Contacto</p>
         <p className='paragraph'><span><FaPhoneAlt /></span>+54 1132816522</p>
         <p className='paragraph'><span><FaPhoneAlt /></span>+54 1130611328</p>
-        <p className='paragraph'><span><FaRegEnvelope /></span>globalincmkt@gmail.com</p>
+        <p className='paragraph'><span><FaRegEnvelope /></span>globalincmkt@ gmail.com</p>
         <p className='paragraph'><span><FaMapMarkerAlt /></span>Buenos Aires, Argentina</p>
       </div>
 
--- src/components/Heros/HeroTop.jsx
+++ src/components/Heros/HeroTop.jsx
@@ -8,7 +8,8 @@ const HeroTop = () => {
     <div className='hero-top-container' id='HeroTop'>
       <div className='hero-top-layout'>
         <div className='hero-top-text'>
-          <h2>Social media is the <span>new black.</span></h2>
+          <h2>Social media is the</h2>
+          <h2><span>new black.</span></h2>
           <div className='paragraph'>
             <p>Hoy en día la presencia en los <b>medios sociales</b> y plataformas digitales se volvieron <b>fundamentales.</b></p>
             <p>Con la estrategia de comunicación correcta, y el equipo correcto <b>-nosotros-</b> podes hacer crecer tu público como nunca te imaginaste.</p>
