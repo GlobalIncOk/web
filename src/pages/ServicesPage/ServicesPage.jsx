b45f7ecf0c0a3df7610d234de63590ef7f6dadc2
cfooostdijk <cfooostdijk@gmail.com>
2023-04-08 13:02:37 -0300

    services page

---
+++ src/pages/ServicesPage/ServicesPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { HeroBottom } from '../../components'
+import { Footer, Header } from '../../containers'
+
+const ServicesPage = () => {
+  return (
+    <>
+      <Header />
+      <HeroBottom />
+      <Footer />
+    </>
+  )
+}
+
+export default ServicesPage
